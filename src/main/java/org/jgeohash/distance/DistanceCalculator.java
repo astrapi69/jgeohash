30e3099c195e7dfd3c2d2b2542f00a5d347e7232
astrapi69 <asterios.raptis@gmx.net>
2013-09-14 18:11:47 +0200

    Refactoring from DistanceCalculator class for giving the ability to calculate distance over latitude and longitude values. Javadoc added.

--- src/main/java/org/jgeohash/distance/DistanceCalculator.java
+++ src/main/java/org/jgeohash/distance/DistanceCalculator.java
@@ -3,37 +3,65 @@ package org.jgeohash.distance;
 import org.jgeohash.GeoHashPoint;
 import org.jgeohash.api.Position;
 
+import static java.lang.Math.*;
+
 /**
- * The Class DistanceCalculator provides methods for calculate the distance between two positions.
+ * The Class DistanceCalculator provides methods for calculate the distance between two geohash, position or coordinate values.
  */
 public class DistanceCalculator {
-	
+		
 	/**
 	 * Returns the distance between two points.
 	 * 
-	 * @param point1
-	 *            the first position.
-	 * @param point2
-	 *            the second position.
+	 * @param start
+	 *            the start position.
+	 * @param end
+	 *            the end position.
 	 * @param unit
 	 *            the measurement unit
 	 * @return the distance between points
 	 */
-	public static double distanceBetweenPoints(final Position point1,
-			final Position point2, MeasuringUnit unit) {
-		double theta = Math.toRadians(point1.getLongitude() - point2.getLongitude());		
-		double latitudePoint1=Math.toRadians(point1.getLatitude());
-		double latitudePoint2=Math.toRadians(point2.getLatitude());		
-		double distance = Math.sin(latitudePoint1) * Math.sin(latitudePoint2) + Math.cos(latitudePoint1) * Math.cos(latitudePoint2)*Math.cos(theta);
+	public static double distanceBetweenPoints(final Position start,
+			final Position end, MeasuringUnit unit) {
+		double distance = distance( 
+				start.getLatitude(), 
+				start.getLongitude(), 
+				end.getLatitude(), 
+				end.getLongitude(),  
+				unit);
+		return distance;
+	}
+
+
+
+	/**
+	 * Returns the distance from the given coordinates values.
+	 *
+	 * @param startLatitude the latitude from start point.
+	 * @param startLongitude the longitude from start point.
+	 * @param endLatitude the latitude from end point.
+	 * @param endLongitude the longitude from end point.
+	 * @param unit 
+	 *            the measurement unit
+	 * @return the distance between the given coordinates values.
+	 */
+	public static double distance(double startLatitude, double startLongitude, double endLatitude, double endLongitude, MeasuringUnit unit) {
+		double theta = toRadians(startLongitude - endLongitude);		
+		double latitudePoint1=toRadians(startLatitude);
+		double latitudePoint2=toRadians(endLatitude);		
+		double distance = sin(latitudePoint1) * sin(latitudePoint2) + cos(latitudePoint1) * cos(latitudePoint2)*cos(theta);
 		// 60 is the number of minutes in a degree
 		// 1.1515 is the number of statute miles in a nautical mile
 		// One nautical mile is the length of one minute of latitude at the equator.
-		distance = Math.toDegrees(Math.acos(distance)) * 60 * 1.1515;		
+		distance = toDegrees(acos(distance)) * 60 * 1.1515;		
 		switch (unit) {
 		case MILE:
 			distance = distance * unit.getFactor();
 			break;
 		case KILOMETER:
+			distance = distance * unit.getFactor();
+			break;
+		case METER:
 			distance = distance * unit.getFactor();
 			break;
 		default:
@@ -46,19 +74,19 @@ public class DistanceCalculator {
 
 	
 	/**
-	 * Returns the distance between two given geohash points.
+	 * Returns the distance between two given geohash values.
 	 * 
-	 * @param point1
-	 *            the first geohash value.
-	 * @param point2
-	 *            the second geohash value.
+	 * @param startGeohashValue
+	 *            the geohash value from where to start the measurement.
+	 * @param endGeohashValue
+	 *            the geohash value where to end the measurement.
 	 * @param unit
 	 *            the measurement unit
-	 * @return the distance between points
+	 * @return the distance between the given geohash values.
 	 */
-	public static double distanceBetweenPoints(final String point1,
-			final String point2, MeasuringUnit unit) {
-		return distanceBetweenPoints(new GeoHashPoint(point1), new GeoHashPoint(point2), unit);
+	public static double distanceBetweenPoints(final String startGeohashValue,
+			final String endGeohashValue, MeasuringUnit unit) {
+		return distanceBetweenPoints(new GeoHashPoint(startGeohashValue), new GeoHashPoint(endGeohashValue), unit);
 	}
 
 }
