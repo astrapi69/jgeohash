960d2fe69c548ef4af33fcc18e74c8ec15f73d4c
astrapi69 <asterios.raptis@gmx.net>
2015-08-29 11:47:42 +0200

    extended unit tests for new feature sort by distance

--- src/test/java/de/alpharogroup/jgeohash/geoip/LocationUtilsTest.java
+++ src/test/java/de/alpharogroup/jgeohash/geoip/LocationUtilsTest.java
@@ -16,11 +16,20 @@ public class LocationUtilsTest
 	@Test
 	public void testSortByDistance()
 	{
-		final List<Position> geohashes = new ArrayList<>();
+		List<Position> geohashes = new ArrayList<>();
 		geohashes.add(new GeoHashPoint("u0vf2w1s5tsy"));
 		geohashes.add(new GeoHashPoint("u0t3z7cnznrx"));
 		geohashes.add(new GeoHashPoint("u0td0v9xnz28"));
-		final Position startPoint = new GeoHashPoint("u11fyhzqhp3c");
+		Position startPoint = new GeoHashPoint("u11fyhzqhp3c");
+
+		LocationUtils.sortByDistance(startPoint, geohashes, MeasuringUnit.METER);
+
+		geohashes = new ArrayList<>();
+		geohashes.add(new GeoHashPoint("u0z4")); // below Wuerzburg
+		geohashes.add(new GeoHashPoint("u1p1")); // right from Bad Hersfeld
+		geohashes.add(new GeoHashPoint("u28j")); // Ingolstadt
+		geohashes.add(new GeoHashPoint("u0zc4wp0k")); // Nurremberg at work
+		startPoint = new GeoHashPoint("u0ww"); // Ludwigsburg
 
 		LocationUtils.sortByDistance(startPoint, geohashes, MeasuringUnit.METER);
 	}
